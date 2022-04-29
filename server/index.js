05108d9ba42136cffe412f8093a118b56e0276f9
Tina <tina.zhou.hui@gmail.com>
2022-04-30 00:30:05 +0100

    feat: swagger implementation

--- server/index.js
+++ server/index.js
@@ -1,12 +1,29 @@
-require('dotenv').config()
+require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
-const logger = require('morgan')
+const logger = require('morgan');
 const router = require('./router');
-const PORT = process.env.PORT;
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
 
+const PORT = process.env.PORT;
 const app = express();
+
+// SWAGGER
+const options = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'uspace API docs',
+      version: '1.0.0',
+    }
+  },
+  apis: ['./router.js'], // files containing annotations as above
+};
+
+const openapiSpecification = swaggerJsdoc(options);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
 
 app.use(cors());
 app.use(express.json());
