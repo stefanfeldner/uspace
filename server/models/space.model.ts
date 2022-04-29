c1ca2a030a15b9d703635a3c30f76656ddb9e8c2
Alex Nussbaumer <alex.nssbmr@gmail.com>
2022-04-29 17:22:29 +0200

    returnspacedata in model done

--- server/models/space.model.ts
+++ server/models/space.model.ts
@@ -19,62 +19,74 @@ const createSpace = async (newSpaceDetails: Space): Promise<ISpace> => {
 };
 
 const returnSpaceData = async (id: number): Promise<ISpaceData | null> => {
-  const spaceData = await prisma.space.findUnique({
-    where: {
-      id: +id
-    },
-    include: {
-      Post: {
-        include: {
-          Comment: true
-        }
+  try {
+    const spaceData = await prisma.space.findUnique({
+      where: {
+        id: +id
       },
-      User_Space_Role: {
-        where: {
-          role_id: 2
+      include: {
+        Post: {
+          include: {
+            Comment: true
+          }
         },
-        select: {
-          user: {
-            select: {
-              username: true,
-              email: true,
-              picture_url: true,
-              id: true
+        User_Space_Role: {
+          where: {
+            role_id: 2
+          },
+          select: {
+            user: {
+              select: {
+                username: true,
+                email: true,
+                picture_url: true,
+                id: true
+              }
             }
           }
         }
       }
+    });
+    if (!spaceData) {
+      return null;
     }
-  });
-  if (!spaceData) {
-    return null;
-  }
-  const sdata: ISpaceData = {
-    id: spaceData.id,
-    name: spaceData.name,
-    description: spaceData.description,
-    createdAt: spaceData.created_at,
-    posts: spaceData.Post.map<IPostWithComments>((post: Post & { Comment: Comment[] }) => ({
-      id: post.id,
-      title: post.title,
-      userId: post.user_id,
-      spaceId: post.space_id,
-      createdAt: post.created_at,
-      content: post.content,
-      tags: post.tags,
-      comments: post.Comment.map<IComment>((comment: Comment) => ({ id: comment.id, content: comment.content, createdAt: comment.created_at, postId: comment.post_id, userId: comment.user_id }))
-    })),
-    userSpaceRoles: spaceData.User_Space_Role.map<{user: IStrippedUser}>((userSpaceRole) => ({
+    const sdata: ISpaceData = {
+      id: spaceData.id,
+      name: spaceData.name,
+      description: spaceData.description,
+      createdAt: spaceData.created_at,
+      posts: spaceData.Post.map<IPostWithComments>((post: Post & { Comment: Comment[] }) => ({
+        id: post.id,
+        title: post.title,
+        userId: post.user_id,
+        spaceId: post.space_id,
+        createdAt: post.created_at,
+        content: post.content,
+        tags: post.tags,
+        comments: post.Comment.map<IComment>((comment: Comment) => ({ id: comment.id, content: comment.content, createdAt: comment.created_at, postId: comment.post_id, userId: comment.user_id }))
+      })),
+      userSpaceRoles: spaceData.User_Space_Role.map<{user: IStrippedUser}>((userSpaceRole: {
       user: {
-        id: userSpaceRole.user.id,
-        username: userSpaceRole.user.username,
-        email: userSpaceRole.user.email,
-        pictureUrl: userSpaceRole.user.picture_url
+          id: number;
+          username: string;
+          email: string;
+          picture_url: string;
+      };
+  }) => ({
+        user: {
+          id: userSpaceRole.user.id,
+          username: userSpaceRole.user.username,
+          email: userSpaceRole.user.email,
+          pictureUrl: userSpaceRole.user.picture_url
 
-      }
-    }))
-  };
-  return sdata;
+        }
+      }))
+    };
+    return sdata;
+  } catch (error) {
+    console.log(error);
+    throw new Error('A database error has occurred.');
+  }
 };
 
 // delete single space and posts/comments inside
