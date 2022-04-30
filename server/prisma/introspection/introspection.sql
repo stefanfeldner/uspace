72ab4b53e16ad2185e49307033dd70b28a347517
thaistcosta <thais_costa31@hotmail.com>
2022-04-30 12:32:06 +0200

    added join tables and genereted new model

--- server/prisma/introspection/introspection.sql
+++ server/prisma/introspection/introspection.sql
@@ -7,19 +7,20 @@ CREATE TABLE legacy.user (
     email_verified BOOLEAN,                
     username VARCHAR( 50 ) NOT NULL,
     picture_url VARCHAR ( 255 ), 
-    sub VARCHAR ( 255 ) PRIMARY KEY NOT NULL, 
-    created_at TIMESTAMP NOT NULL
+    sub VARCHAR ( 255 ) NOT NULL, 
+    user_id SERIAL PRIMARY KEY,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
 -- CreateTable
 CREATE TABLE legacy.space (
     space_id SERIAL PRIMARY KEY NOT NULL,    
-    user_id VARCHAR ( 255 ) NOT NULL,    
+    user_id INTEGER NOT NULL,    
     name TEXT NOT NULL,
     description TEXT,
-    created_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id)
-        REFERENCES legacy.user(sub)
+        REFERENCES legacy.user(user_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- CreateTable
@@ -29,37 +30,59 @@ CREATE TABLE legacy.post (
     title  TEXT NOT NULL,
     tags TEXT NOT NULL,
     content TEXT,
-    created_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (space_id)
-        REFERENCES legacy.space(space_id)
+        REFERENCES legacy.space(space_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- CreateTable
 CREATE TABLE legacy.comment (
     comment_id SERIAL PRIMARY KEY NOT NULL,
     content TEXT NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    user_id VARCHAR ( 255 ) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    user_id INTEGER NOT NULL,
     post_id INTEGER NOT NULL,
     FOREIGN KEY (user_id)
-        REFERENCES legacy.user(sub),
+        REFERENCES legacy.user(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (post_id)
-        REFERENCES legacy.post(post_id)
+        REFERENCES legacy.post(post_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- CreateTable
 CREATE TABLE legacy.space_colab (
     space_colab_id SERIAL PRIMARY KEY NOT NULL,
     space_id INTEGER NOT NULL,
-    user_id VARCHAR ( 255 ) NOT NULL,
+    user_id INTEGER NOT NULL,
     post_id INTEGER NOT NULL,
     canWrite BOOLEAN NOT NULL DEFAULT FALSE,
     FOREIGN KEY (space_id)
-        REFERENCES legacy.space(space_id),
+        REFERENCES legacy.space(space_id) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (user_id)
-        REFERENCES legacy.user(sub),
+        REFERENCES legacy.user(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (post_id)
-        REFERENCES legacy.post(post_id)
+        REFERENCES legacy.post(post_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
+-- CreateJoinTable
+CREATE TABLE legacy._SpaceUser (
+    A integer NOT NULL REFERENCES legacy.space(space_id) ,
+    B integer NOT NULL REFERENCES legacy.user(user_id)
+);
+CREATE UNIQUE INDEX _SpaceUser_AB_unique ON legacy._SpaceUser (A int4_ops,B int4_ops);
+CREATE INDEX _SpaceUser_B_index ON legacy._SpaceUser(B int4_ops);
 
+-- CreateJoinTable
+CREATE TABLE legacy._PostSpace (
+    A integer NOT NULL REFERENCES legacy.post(post_id) ,
+    B integer NOT NULL REFERENCES legacy.space(space_id)
+);
+CREATE UNIQUE INDEX _PostSpace_AB_unique ON legacy._PostSpace(A int4_ops,B int4_ops);
+CREATE INDEX _PostSpace_B_index ON legacy._PostSpace(B int4_ops);
+
+-- CreateJoinTable
+CREATE TABLE legacy._CommentPost (
+    A integer NOT NULL REFERENCES legacy.comment(comment_id) ,
+    B integer NOT NULL REFERENCES legacy.user(user_id)
+);
+CREATE UNIQUE INDEX _CommentPost_AB_unique ON legacy._CommentPost(A int4_ops,B int4_ops);
+CREATE INDEX _CommentPost_B_index ON legacy._CommentPost(B int4_ops);
