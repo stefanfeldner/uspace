174469199f873dd0ea16d0bcb81bedc3401cd93b
Gabe <glev2190@gmail.com>
2022-04-30 13:41:49 +0200

    c2t both the comment model and the controller!

---
+++ server/models/comment.model.ts
@@ -0,0 +1,26 @@
+import { Comment, PrismaClient } from '@prisma/client';
+import { IComment } from '../interfaces/comment.interface';
+
+const prisma = new PrismaClient();
+
+// creates a single comment
+export const createComment = async (commentDetails: Comment): Promise<IComment> => {
+  try {
+    // const { content, user_id: userId, post_id: postId } = commentDetails;
+
+    const comment = await prisma.comment.create({
+      data: commentDetails
+    });
+
+    return {
+      id: comment.id,
+      content: comment.content,
+      createdAt: comment.created_at,
+      userId: comment.user_id,
+      postId: comment.post_id
+    };
+  } catch (error) {
+    console.log('Error in createComment', error);
+    throw new Error('A database error has occured');
+  }
+};
