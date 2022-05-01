9eeef9c55f2240507ec3eb5ddb93a046f04037a0
thaistcosta <thais_costa31@hotmail.com>
2022-05-01 16:35:34 +0200

    added test and types of jest

--- server/src/__test__/index.test.ts
+++ server/src/__test__/index.test.ts
@@ -1,20 +1,41 @@
 import app from '../app'; 
 import request from 'supertest';
+import { describe, expect, it, beforeEach, afterEach} from '@jest/globals'
 
-let server: any; 
+let server!: any; 
+
 
 beforeEach(async() => {
-  server = await app.listen(3005)
-})
+  server = await app.listen(3001)
+});
 
 afterEach(() => {
   server.close();
 })
 
+
 it('simple test', ()=> {
   expect(1+1).toBe(2);
 })
 
+describe('GET/', () => {
+    describe('when the user login in GET/users/:sub', () => {
+        it('Get User Request', async () => {
+          const output = await request(app).get('/users');
+          console.log(output.body)
+          expect(output.statusCode).toEqual(200);
+        })
+    })
+})
+
+describe('GET/', () => {
+    describe('when the user login in GET/users/:sub', () => {
+        it('Get User Request', async () => {
+          const output = await request(app).get('/users');
+          expect(output.statusCode).toEqual(200);
+        })
+    })
+})
 
 describe('GET/', () => {
     describe('when the user login in GET/users/:sub', () => {
