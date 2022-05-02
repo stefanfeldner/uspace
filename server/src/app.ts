86b28b4121f8bf4e88e11fae436f3789dec00c5f
Tina <tina.zhou.hui@gmail.com>
2022-05-02 22:25:38 +0100

    fix: openapi validation

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,15 +4,29 @@ import morgan from 'morgan';
 import router from './router';
 import swaggerUi from 'swagger-ui-express';
 import openapiSpecification from './docs/swaggerDef';
+import * as OpenApiValidator from 'express-openapi-validator';
+import fs from 'fs';
 
 const app: Application  = express();
 
 
 // SWAGGER
+fs.writeFile('openapi.json', JSON.stringify(openapiSpecification), (err) => {});
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification))
 
 app.use(cors());
 app.use(express.json());
+
+// OPENAPI VALIDATION
+app.use(
+  OpenApiValidator.middleware({
+      // @ts-ignore
+    apiSpec: openapiSpecification,
+    validateRequests: true, // (default)
+    // validateResponses: true, // false by default
+  }),
+);
+
 app.use(morgan('dev'));
 app.use(router);
 
