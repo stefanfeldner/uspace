09e8c53e29dde8d20ce6d704b952ee1ee31a13c0
Alex Nussbaumer <alex.nssbmr@gmail.com>
2022-05-02 16:33:30 +0200

    adapted user route to match camelcase

--- server/queries/user.queries.ts
+++ server/queries/user.queries.ts
@@ -2,8 +2,16 @@ import { User } from '@prisma/client';
 import { IIncomingUser } from '../interfaces/user.interface';
 import { prisma } from '../prisma/prisma-client';
 
-export const createUserQuery = async (newUserDetails : IIncomingUser): Promise<User> => await prisma.user.create(
-  { data: newUserDetails }
+export const createUserQuery = async (newUserDetails: IIncomingUser): Promise<User> => await prisma.user.create(
+  {
+    data: {
+      email: newUserDetails.email,
+      picture_url: newUserDetails.pictureUrl,
+      username: newUserDetails.username,
+      email_verified: newUserDetails.emailVerified ?? false,
+      sub: newUserDetails.sub
+    }
+  }
 );
 
 export const findFirstQuery = async (sub: string): Promise<User | null> => {
