298f2b0e54a420d6ce6e451736f92541c6697fc8
Gabe <glev2190@gmail.com>
2022-05-03 13:20:20 +0200

    completed controller tests

--- server/tests/unit-tests/controller-tests/modular-routes.controller.test.ts
+++ server/tests/unit-tests/controller-tests/modular-routes.controller.test.ts
@@ -1,35 +1,34 @@
-import { getAllEntries } from '../../../controllers/modular-routes.controller';
-// import { IComment } from '../../../interfaces/comment.interface';
-// import { IPost } from '../../../interfaces/post.interface';
-// import { IRole } from '../../../interfaces/role.interface';
-// import { ISpace } from '../../../interfaces/space.interface';
-// import { IUserSpaceRole } from '../../../interfaces/user-space-role.interface';
-// import { IUser } from '../../../interfaces/user.interface';
+import { getAllEntries, getSingleEntryById } from '../../../controllers/modular-routes.controller';
 import { Request, Response } from 'express';
 
-// type IAllTables = IComment | IPost | IRole | ISpace | IUserSpaceRole | IUser;
 const tableNameList = ['Comment', 'Post', 'Space', 'User_Space_Role', 'User'];
+const tableNameListWithRole = ['Comment', 'Post', 'Space', 'User_Space_Role', 'User', 'Role'];
 let MOCK_TABLE_NAME;
 const MOCK_ID = '3';
 const MOCK_RES_ARR = [{ id: +MOCK_ID, tableName: MOCK_TABLE_NAME }, { id: +MOCK_ID + 1, tableName: MOCK_TABLE_NAME }];
-// const MOCK_RES = { body: { MOCK_RES_ARR }, id: 1 };
 
 jest.mock('../../../models/modular-routes.model', () => ({
   returnAllEntries: (tableName: string): any => {
     const tableNameIndex = tableNameList.indexOf(tableName);
-    if (tableNameIndex >= 0) { return MOCK_RES_ARR; } else if (tableName === 'Role') {
-      return null;
-    } else { throw new Error(); }
+    if (tableNameIndex >= 0) { return MOCK_RES_ARR; }
+    if (tableName === 'Role') { return null; }
+    throw new Error();
+  },
+  returnEntryById: (route: string, id: number): any => {
+    const tableNameIndex = tableNameListWithRole.indexOf(route);
+    if (+MOCK_ID === id && tableNameIndex >= 0) { return { ...MOCK_RES_ARR[0], tableName: route }; }
+    if (id !== +MOCK_ID && typeof id === 'number') { return []; }
+    throw new Error('A database error has occurred.');
   }
 }));
 
 describe.only('Testing Modular-Routes Controller', () => {
+  // Testing Get all entries
   it('Should send request to model and fetch all results for Comment', async () => {
     MOCK_TABLE_NAME = 'Comment';
     const MOCK_REQ = { path: '/' + MOCK_TABLE_NAME + 's' };
     const mReq = MOCK_REQ as any as Request;
     const mRes = { status: jest.fn().mockReturnThis(), send: jest.fn() } as any as Response;
-    console.log('mReq.path.slice(1, -1)', mReq.path.slice(1, -1));
     await getAllEntries(mReq, mRes);
     expect(mRes.status).toBeCalledWith(200);
     expect(mRes.send).toBeCalledWith(MOCK_RES_ARR);
@@ -39,7 +38,6 @@ describe.only('Testing Modular-Routes Controller', () => {
     const MOCK_REQ = { path: '/' + MOCK_TABLE_NAME + 's' };
     const mReq = MOCK_REQ as any as Request;
     const mRes = { status: jest.fn().mockReturnThis(), send: jest.fn() } as any as Response;
-    console.log('mReq.path.slice(1, -1)', mReq.path.slice(1, -1));
     await getAllEntries(mReq, mRes);
     expect(mRes.status).toBeCalledWith(200);
     expect(mRes.send).toBeCalledWith(MOCK_RES_ARR);
@@ -49,7 +47,6 @@ describe.only('Testing Modular-Routes Controller', () => {
     const MOCK_REQ = { path: '/' + MOCK_TABLE_NAME + 's' };
     const mReq = MOCK_REQ as any as Request;
     const mRes = { status: jest.fn().mockReturnThis(), send: jest.fn() } as any as Response;
-    console.log('mReq.path.slice(1, -1)', mReq.path.slice(1, -1));
     await getAllEntries(mReq, mRes);
     expect(mRes.status).toBeCalledWith(200);
     expect(mRes.send).toBeCalledWith(MOCK_RES_ARR);
@@ -59,7 +56,6 @@ describe.only('Testing Modular-Routes Controller', () => {
     const MOCK_REQ = { path: '/' + MOCK_TABLE_NAME + 's' };
     const mReq = MOCK_REQ as any as Request;
     const mRes = { status: jest.fn().mockReturnThis(), send: jest.fn() } as any as Response;
-    console.log('mReq.path.slice(1, -1)', mReq.path.slice(1, -1));
     await getAllEntries(mReq, mRes);
     expect(mRes.status).toBeCalledWith(200);
     expect(mRes.send).toBeCalledWith(MOCK_RES_ARR);
@@ -69,7 +65,6 @@ describe.only('Testing Modular-Routes Controller', () => {
     const MOCK_REQ = { path: '/' + MOCK_TABLE_NAME + 's' };
     const mReq = MOCK_REQ as any as Request;
     const mRes = { status: jest.fn().mockReturnThis(), send: jest.fn() } as any as Response;
-    console.log('mReq.path.slice(1, -1)', mReq.path.slice(1, -1));
     await getAllEntries(mReq, mRes);
     expect(mRes.status).toBeCalledWith(200);
     expect(mRes.send).toBeCalledWith(MOCK_RES_ARR);
@@ -79,18 +74,91 @@ describe.only('Testing Modular-Routes Controller', () => {
     const MOCK_REQ = { path: '/' + MOCK_TABLE_NAME + 's' };
     const mReq = MOCK_REQ as any as Request;
     const mRes = { status: jest.fn().mockReturnThis(), send: jest.fn() } as any as Response;
-    console.log('mReq.path.slice(1, -1)', mReq.path.slice(1, -1));
     await getAllEntries(mReq, mRes);
     expect(mRes.status).toBeCalledWith(200);
     expect(mRes.send).toBeCalledWith(null);
   });
-  it('Should return null if the model is passed a request for Role', async () => {
-    MOCK_TABLE_NAME = 'WRONGPATH';
+  it('Should return error if the model is passed a request for Wrong Path', async () => {
+    MOCK_TABLE_NAME = 'WRONG PATH';
     const MOCK_REQ = { path: '/' + MOCK_TABLE_NAME + 's' };
     const mReq = MOCK_REQ as any as Request;
     const mRes = { status: jest.fn().mockReturnThis(), send: jest.fn() } as any as Response;
-    console.log('mReq.path.slice(1, -1)', mReq.path.slice(1, -1));
     await getAllEntries(mReq, mRes);
+    expect(mRes.status).toBeCalledWith(500);
+    expect(mRes.send).toBeCalledWith({ error: 'An unknown server error has occurred.' });
+  });
+
+  // Testing getSingleEntryByID
+  it('Should send request to model and fetch the result with corresponding id for Comment', async () => {
+    MOCK_TABLE_NAME = 'Comment';
+    const MOCK_REQ = { path: MOCK_TABLE_NAME, params: { id: +MOCK_ID } };
+    const mReq = MOCK_REQ as any as Request;
+    const mRes = { status: jest.fn().mockReturnThis(), send: jest.fn() } as any as Response;
+    await getSingleEntryById(mReq, mRes);
+    expect(mRes.status).toBeCalledWith(200);
+    expect(mRes.send).toBeCalledWith({ ...MOCK_RES_ARR[0], tableName: MOCK_TABLE_NAME });
+  });
+  it('Should send request to model and fetch the result with corresponding id for Post', async () => {
+    MOCK_TABLE_NAME = 'Post';
+    const MOCK_REQ = { path: MOCK_TABLE_NAME, params: { id: +MOCK_ID } };
+    const mReq = MOCK_REQ as any as Request;
+    const mRes = { status: jest.fn().mockReturnThis(), send: jest.fn() } as any as Response;
+    await getSingleEntryById(mReq, mRes);
+    expect(mRes.status).toBeCalledWith(200);
+    expect(mRes.send).toBeCalledWith({ ...MOCK_RES_ARR[0], tableName: MOCK_TABLE_NAME });
+  });
+  it('Should send request to model and fetch the result with corresponding id for Space', async () => {
+    MOCK_TABLE_NAME = 'Space';
+    const MOCK_REQ = { path: MOCK_TABLE_NAME, params: { id: +MOCK_ID } };
+    const mReq = MOCK_REQ as any as Request;
+    const mRes = { status: jest.fn().mockReturnThis(), send: jest.fn() } as any as Response;
+    await getSingleEntryById(mReq, mRes);
+    expect(mRes.status).toBeCalledWith(200);
+    expect(mRes.send).toBeCalledWith({ ...MOCK_RES_ARR[0], tableName: MOCK_TABLE_NAME });
+  });
+  it('Should send request to model and fetch the result with corresponding id for User_Space_Role', async () => {
+    MOCK_TABLE_NAME = 'User_Space_Role';
+    const MOCK_REQ = { path: MOCK_TABLE_NAME, params: { id: +MOCK_ID } };
+    const mReq = MOCK_REQ as any as Request;
+    const mRes = { status: jest.fn().mockReturnThis(), send: jest.fn() } as any as Response;
+    await getSingleEntryById(mReq, mRes);
+    expect(mRes.status).toBeCalledWith(200);
+    expect(mRes.send).toBeCalledWith({ ...MOCK_RES_ARR[0], tableName: MOCK_TABLE_NAME });
+  });
+  it('Should send request to model and fetch the result with corresponding id for User', async () => {
+    MOCK_TABLE_NAME = 'User';
+    const MOCK_REQ = { path: MOCK_TABLE_NAME, params: { id: +MOCK_ID } };
+    const mReq = MOCK_REQ as any as Request;
+    const mRes = { status: jest.fn().mockReturnThis(), send: jest.fn() } as any as Response;
+    await getSingleEntryById(mReq, mRes);
+    expect(mRes.status).toBeCalledWith(200);
+    expect(mRes.send).toBeCalledWith({ ...MOCK_RES_ARR[0], tableName: MOCK_TABLE_NAME });
+  });
+  it('Should send request to model and fetch the result with corresponding id for Role', async () => {
+    MOCK_TABLE_NAME = 'Role';
+    const MOCK_REQ = { path: MOCK_TABLE_NAME, params: { id: +MOCK_ID } };
+    const mReq = MOCK_REQ as any as Request;
+    const mRes = { status: jest.fn().mockReturnThis(), send: jest.fn() } as any as Response;
+    await getSingleEntryById(mReq, mRes);
+    expect(mRes.status).toBeCalledWith(200);
+    expect(mRes.send).toBeCalledWith({ ...MOCK_RES_ARR[0], tableName: MOCK_TABLE_NAME });
+  });
+  it('Should send request to model and return empty array if entry not in database', async () => {
+    MOCK_TABLE_NAME = 'Role';
+    const MOCK_ID_OFFSET = 10;
+    const MOCK_REQ = { path: MOCK_TABLE_NAME, params: { id: +MOCK_ID + MOCK_ID_OFFSET } };
+    const mReq = MOCK_REQ as any as Request;
+    const mRes = { status: jest.fn().mockReturnThis(), send: jest.fn() } as any as Response;
+    await getSingleEntryById(mReq, mRes);
+    expect(mRes.status).toBeCalledWith(200);
+    expect(mRes.send).toBeCalledWith([]);
+  });
+  it('Should send request to model and throw error if route is wrong', async () => {
+    MOCK_TABLE_NAME = 'WRONG ROUTE';
+    const MOCK_REQ = { path: MOCK_TABLE_NAME, params: { id: +MOCK_ID } };
+    const mReq = MOCK_REQ as any as Request;
+    const mRes = { status: jest.fn().mockReturnThis(), send: jest.fn() } as any as Response;
+    await getSingleEntryById(mReq, mRes);
     expect(mRes.status).toBeCalledWith(500);
     expect(mRes.send).toBeCalledWith({ error: 'An unknown server error has occurred.' });
   });
