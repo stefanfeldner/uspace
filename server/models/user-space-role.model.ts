6a244b5e1fe9c35edaa91e80bfe52f82dfef6d9c
ms-aija <aija.avota@gmail.com>
2022-05-03 12:34:35 +0200

    moved returnSpacesAndCreators db query to a dedicated queries function

--- server/models/user-space-role.model.ts
+++ server/models/user-space-role.model.ts
@@ -1,12 +1,7 @@
-// eslint-disable-next-line camelcase
-import { PrismaClient } from '@prisma/client';
 import { CustomError } from '../error-handling/custom-err.class';
-import { IStrippedUser } from '../interfaces/spaceData.interface';
 import { ISpacesAndCreator } from '../interfaces/spaces-and-creators.interface';
 import { IIncomingUserSpaceRole, IUserSpaceRole } from '../interfaces/user-space-role.interface';
-import { createUserSpaceRoleQuery, deleteSingleUserSpaceRoleQuery } from '../queries/user-space-role.queries';
-
-const prisma = new PrismaClient();
+import { createUserSpaceRoleQuery, deleteSingleUserSpaceRoleQuery, returnSpacesAndCreatorsQuery } from '../queries/user-space-role.queries';
 
 // creates a single User_Space_Role
 // eslint-disable-next-line camelcase
@@ -22,39 +17,8 @@ export const createUserSpaceRole = async (newUserSpaceRoleDetails: IIncomingUser
 
 export const returnSpacesAndCreators = async (): Promise<ISpacesAndCreator[]> => {
   try {
-    const spacesAndCreators = await prisma.space.findMany({
-      include: {
-        User_Space_Role: {
-          where: {
-            role_id: 2
-          },
-          select: {
-            user: {
-              select: {
-                id: true,
-                username: true,
-                email: true,
-                picture_url: true
-              }
-            }
-          }
-        }
-      }
-    });
-    return spacesAndCreators.map<ISpacesAndCreator>((spaceAndCreator) => ({
-      id: spaceAndCreator.id,
-      name: spaceAndCreator.name,
-      description: spaceAndCreator.description,
-      createdAt: spaceAndCreator.created_at,
-      userSpaceRoles: spaceAndCreator.User_Space_Role.map<{user: IStrippedUser}>((userSpaceRole) => ({
-        user: {
-          id: userSpaceRole.user.id,
-          email: userSpaceRole.user.email,
-          username: userSpaceRole.user.username,
-          pictureUrl: userSpaceRole.user.picture_url
-        }
-      }))
-    }));
+    const spacesAndCreators = await returnSpacesAndCreatorsQuery();
+    return spacesAndCreators;
   } catch (error) {
     console.log(error);
     throw new CustomError('A database error has occurred.');
--- server/queries/user-space-role.queries.ts
+++ server/queries/user-space-role.queries.ts
@@ -1,5 +1,6 @@
 // eslint-disable-next-line camelcase
 import { User_Space_Role } from '@prisma/client';
+import { ISpacesAndCreator } from '../interfaces/spaces-and-creators.interface';
 import { IIncomingUserSpaceRole } from '../interfaces/user-space-role.interface';
 import { prisma } from '../prisma/prisma-client';
 
@@ -8,10 +9,35 @@ export const createUserSpaceRoleQuery = async (newUserSpaceRoleDetails: IIncomin
   data: newUserSpaceRoleDetails
 });
 
-// export const returnSpacesAndCreatorsQuery = async () => prisma.({
-//   data:
-// });
-
+export const returnSpacesAndCreatorsQuery = async (): Promise<ISpacesAndCreator[]> => {
+  const dbSpacesAndCreators = await prisma.space.findMany({
+    include: {
+      User_Space_Role: {
+        where: {
+          role_id: 2
+        },
+        select: {
+          user: {
+            select: {
+              id: true,
+              username: true,
+              email: true,
+              picture_url: true
+            }
+          }
+        }
+      }
+    }
+  });
+  const spacesAndCreators: ISpacesAndCreator[] = dbSpacesAndCreators.map<ISpacesAndCreator>((spaceAndCreator) => ({
+    createdAt: spaceAndCreator.created_at,
+    id: spaceAndCreator.id,
+    description: spaceAndCreator.description,
+    name: spaceAndCreator.name,
+    userSpaceRoles: spaceAndCreator.User_Space_Role.map<{user: {id: number, email: string, pictureUrl: string, username: string}}>((dbUserSpaceRole) => ({ user: { id: dbUserSpaceRole.user.id, email: dbUserSpaceRole.user.email, pictureUrl: dbUserSpaceRole.user.picture_url, username: dbUserSpaceRole.user.username } }))
+  }));
+  return spacesAndCreators;
+};
 export const deleteSingleUserSpaceRoleQuery = async (spaceId: string): Promise<any> => prisma.user_Space_Role.deleteMany({
   where: {
     space_id: +spaceId // parse id to int
