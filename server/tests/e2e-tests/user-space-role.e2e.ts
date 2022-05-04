04615130f299f50b2b3cf2bffb258c6cb42c7a7f
Gabe <glev2190@gmail.com>
2022-05-04 16:06:26 +0200

    completed all user space role tests for post and del routes

--- server/tests/e2e-tests/user-space-role.e2e.ts
+++ server/tests/e2e-tests/user-space-role.e2e.ts
@@ -56,9 +56,9 @@ export const userSpaceRoleTests = (): void => {
     });
   });
 
-  describe.only('Delete /User-Space-Roles', () => {
+  describe('Delete /User-Space-Roles', () => {
     let MOCK_SPACE_ID: number;
-    // let MOCK_ID: number;
+    let MOCK_ID: number;
     beforeEach(async () => {
       const MOCK_USER_SPACE_ROLE = await addMockUserSpaceRole();
       const newEntry:any = await prisma.user_Space_Role.findFirst({
@@ -67,7 +67,7 @@ export const userSpaceRoleTests = (): void => {
         }
       });
       MOCK_SPACE_ID = newEntry.space_id;
-      // MOCK_ID = newEntry.id;
+      MOCK_ID = newEntry.id;
     });
     afterEach(async () => {
       await prisma.user_Space_Role.deleteMany();
@@ -84,33 +84,30 @@ export const userSpaceRoleTests = (): void => {
       const postSpace: any = await request(app)
         .delete(`/User_Space_Roles/${MOCK_SPACE_ID}`);
 
-      // const getResponse: any = await prisma.user_Space_Role.findUnique({
-      //   where: {
-      //     id: MOCK_ID
-      //   }
-      // });
-      console.log('MOCK_SPACE_ID', MOCK_SPACE_ID);
-      console.log('postSpace.body', postSpace.body);
-      // console.log('getResponse', getResponse);
+      const getResponse: any = await prisma.user_Space_Role.findUnique({
+        where: {
+          id: MOCK_ID
+        }
+      });
 
-      // expect(getResponse).toEqual(null); // Check to make sure the database no longer holds the given entry
+      expect(getResponse).toEqual(null); // Check to make sure the database no longer holds the given entry
       expect(postSpace.status).toEqual(202); // Check to make sure the status code is correct
       expect(postSpace.body.count).toEqual(1);// Check to make sure the right data is return to frontend after post
     });
 
-    //   it('Should handle failed delete space requests, return a 500 status and send an error message', async () => {
-    //     const postSpace: any = await request(app)
-    //       .delete('/spaces/WRONG_ID');
+    it('Should handle failed delete space requests, return a 500 status and send an error message', async () => {
+      const postSpace: any = await request(app)
+        .delete('/User_Space_Roles/WRONG_ID');
 
-    //     const getResponse: any = await prisma.user_Space_Role.findFirst({
-    //       where: {
-    //         id: MOCK_SPACE_ID
-    //       }
-    //     });
+      const getResponse: any = await prisma.user_Space_Role.findFirst({
+        where: {
+          id: MOCK_ID
+        }
+      });
 
-    //     expect(getResponse.name).toEqual(MOCK_USER_SPACE_ROLE_INCOMING.name); // Check to make sure the database no longer holds the given entry
-    //     expect(postSpace.status).toEqual(500); // Check to make sure the status code is correct
-    //     expect(postSpace.body).toEqual({ error: 'A database error has occurred.' });// Check to make sure the right data is return to frontend after post
-    // });
+      expect(getResponse.id).toEqual(MOCK_ID); // Check to make sure the database no longer holds the given entry
+      expect(postSpace.status).toEqual(500); // Check to make sure the status code is correct
+      expect(postSpace.body).toEqual({ error: 'A database error has occurred.' });// Check to make sure the right error is return to frontend after post
+    });
   });
 };
