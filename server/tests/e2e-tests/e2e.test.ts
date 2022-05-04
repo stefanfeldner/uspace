ec69366fdf276880d384b81703d0e188ef3cc703
Gabe <glev2190@gmail.com>
2022-05-04 12:06:08 +0200

    added test for spaces delete route

--- server/tests/e2e-tests/e2e.test.ts
+++ server/tests/e2e-tests/e2e.test.ts
@@ -1,9 +1,11 @@
 import { commentsTests } from './comments.e2e';
 import { postsTests } from './posts.e2e';
+import { spaceTests } from './spaces.e2e';
 import { userTests } from './users.e2e';
 
 describe('E2E Tests', () => {
   postsTests();
   userTests();
   commentsTests();
+  spaceTests();
 });
