b333b362cc67e61f32cb91f6f48c9b5de782ef79
Alex Nussbaumer <alex.nssbmr@gmail.com>
2022-05-04 15:54:31 +0200

    updated tests

---
+++ server/tests/unit-tests/controller-tests/controller.test.ts
@@ -0,0 +1,15 @@
+import { commentControllerTests } from './comment.controller';
+import { modularRoutesControllerTests } from './modular-routes.controller';
+import { postControllerTests } from './post.controller';
+import { spaceControllerTests } from './space.controller';
+import { userSpaceRoleControllerTests } from './user-space-role.controller';
+import { userControllerTests } from './user.controller';
+
+describe('Controller Tests', () => {
+  commentControllerTests();
+  modularRoutesControllerTests();
+  postControllerTests();
+  spaceControllerTests();
+  userSpaceRoleControllerTests();
+  userControllerTests();
+});
